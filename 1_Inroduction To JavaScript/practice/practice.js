4430571aa5ded6fcda3d6e1ae16383c88bbcced5
your_github_ <your_email@example.com>
2021-01-07 01:28:05 +0200

    Java Script

---
+++ 1_Inroduction To JavaScript/practice/practice.js
@@ -0,0 +1,35 @@
+console.log('Introduction To JavaScript');
+//Practice
+//Predict the value of the following expressions then try them out in the console 
+9 + 1 * 5;  // => 14
+9 % 2;  // => 1
+(9 / 3) * (10 - 7);  // => 9
+21 / 3 + (3 * 9) * 9 + 5;  // => 255
+36 / 9 + 48 - 10 / 2;  // => 47
+
+
+//Write an expression that represents the average grade of an exam, knowing that in a class of ten students, five of them got 24/30, two of them got 16/30 and the remaning ones got 29/30
+
+( 5 * 24 + 2 * 16 + 3 * 29 ) / 10 
+// => 23.9
+
+//Write an expression that represents the number of seconds in 30 days.
+
+60 * 60 * 24 * 30 
+// => 2592000
+
+//Using string concatenation write an expression that represents Your First name, Last name, Gender, and Nationality
+
+'osama' + ' ' +  'salem' + ', ' + 'Male' + ', ' + 'Jordanian'  
+// => "osama salem, Male, Jordanian"
+
+//Before trying the following expressions try to predict the outcome
+
+10 + "10"; // => "1010"
+"10" + "10"; // => "1010"
+10 + 10 + "5"; // => "205"
+"Hello" + 1; // => "Hello1"
+false - true; // => -1
+
+
+ 
