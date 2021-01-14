d7db804a02d69fa73a6eccb01589af37453571d4
your_github_ <your_email@example.com>
2021-01-14 11:43:37 +0200

    7_Iteration in JavaScript

--- 7_Iteration in JavaScript/practice/practice.js
+++ 7_Iteration in JavaScript/practice/practice.js
@@ -33,38 +33,47 @@ for (key in objectt) {
 
 //Practice
 //Write a function sum that accepts an array of numbers and returns the sum of the numbers.
+//اكتب مجموع دالة يقبل مصفوفة من الأرقام ويعيد مجموع الأرقام
 const sum = function (numbers) {
-  let sum = 0
-  for (let i =0; i < numbers.length; i++){
-    sum += numbers[i]
+  let result =0
+  for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i] 
+    
   }
-  return sum
+  return result
 };
-sum([1, 2, 3, 4, 5, 6]); // => 21
-
-
+sum([1, 2, 3, 4, 5, 6]); 
 //Write a function onlyOddNumbers that accepts an array of numbers and returns a new array with only the odd numbers.
+//اكتب وظيفة فقط الأرقام الفردية التي تقبل مصفوفة من الأرقام وتعيد مصفوفة جديدة بالأرقام الفردية فقط.
+
 const filter = function (numbers) {
-  let oddNumbers = []
+  let odd = []
   for (let i =0; i < numbers.length; i++){
     if (numbers[i] % 2 !== 0){
-      oddNumbers.push(numbers[i])
+      odd.push(numbers[i])
     }
   }
-  return oddNumbers;
+  return odd;
 };
+
 filter([0, 1, 2, 3, 4, 5, 6]); // => [1, 3, 5]
 
+
 //Write a function maximumNumber that accepts an array of numbers and returns the max number in the array.
+//اكتب عددًا أقصى للدالة يقبل مصفوفة من الأرقام ويعيد العدد الأقصى في المصفوفة.
+
 const maximumNumber = function (numbers) {
-   let maxNumber = -Infinity
-    for (let i = 0; i < numbers.length; i++){
-      if (numbers[i] > maxNumber){
-      maxNumber = numbers[i]
+  let max =-Infinity
+  for (let i = 0; i < numbers.length; i++) {
+    if(numbers[i] > max)
+    {
+      max = numbers[i]
     }
+    
   }
-  return maxNumber
+  return max
 };
+
 maximumNumber([0, 5, 2, 10, 8, 6]); // => 10
 
 //Write a function totalBill that accepts an object representing separate bills and returns the sum of all bills.
@@ -77,15 +86,16 @@ const bills = {
 };
 
 const totalBill = function (billsObject) {
-  let sum = 0
-  for (let key in billsObject){
+ let sum = 0
+  for( let key in billsObject){
     sum += billsObject[key]
   }
-  return sum;
+  return sum
 };
 totalBill(bills); // => 1075
 
 //Write a function validateMessage that accepts an object representing a message and return the object if it is valid and after removing any extra keys, the message will consist of three keys username, message, date with all of their values as strings return the object only if all three keys are strings, if the number of keys is more than three then delete the extra keys, if the message doesn't have the right data type then return invalid message
+//اكتب رسالة تحقق من صحة الوظيفة تقبل كائنًا يمثل رسالة وتعيد الكائن إذا كان صالحًا وبعد إزالة أي مفاتيح إضافية ، ستتكون الرسالة من ثلاثة مفاتيح اسم المستخدم والرسالة والتاريخ مع جميع قيمها حيث تقوم السلاسل بإرجاع الكائن فقط إذا كانت جميع المفاتيح الثلاثة عبارة عن سلاسل ، وإذا كان عدد المفاتيح أكثر من ثلاثة ، فاحذف المفاتيح الإضافية ، وإذا كانت الرسالة لا تحتوي على نوع البيانات الصحيح ، فقم بإرجاع رسالة غير صالحة
 
 const messageOne = {
   username: "John",
@@ -100,19 +110,21 @@ const messageTwo = {
   date: "01/01/2020",
 };
 
+
 const validateMessage = function (message) {
-  for (let key in message){
-    if ( key !== "username" &&  key !== "message" &&  key !== "date"){
+  for(let key in message)
+  {
+    if( key !== "username" &&  key !== "message" && key !== "date"){
       delete message[key]
     }
-    else {
-      if ( typeof  message[key] !== "string"){
-           return "invalid message" 
-      }
+    else{ 
+    if(typeof message[key] !== "string"){
+          return "invalid message"
     }
   }
-    return message;
+  return message
 };
+}
 validateMessage(messageOne); // => {username: "John", message: "Hello", date:"01/01/2020"}
 validateMessage(messageTwo); // => invalid message
 validateMessage(messageOne); // => {username: "John", message: "Hello", date:"01/01/2020"}
@@ -125,11 +137,11 @@ validateMessage(messageTwo); // => invalid message
     for (let i =0; i < numbers.length; i++){
       if (numbers[i] % 2 !== 0){
         numbers.splice(i, 1)
-        i--
       }
     }
     return numbers
   };
+  
 
 onlyEven([0, 1, 2, 3, 4, 5, 6]) // =>[0, 2, 4, 6]
 onlyEven([1, 9, 2, 3, 4]) // => [2, 4]
@@ -167,13 +179,13 @@ convertToArray({ 0: "one", 1: "two", 2: "three" }); // => ["one", "two", "three"
 //Write a function compare that accepts an array and an object and returns true if all the array values are present as object values.
 
   const compare = function (array, object) {
-    const objVals = {}
+    const obj= {}
   
     for (let key in object){
-      objVals[object[key]] = 1
+      obj[object[key]] = 1
     }
     for (let i = 0; i < array.length; i++){
-      if (!objVals[array[i]]){
+      if (!obj[array[i]]){
         return false
       }
     }
@@ -198,19 +210,16 @@ return object
   deleteKeys(["one", "two"], { "one": "one", "two": "two", "three": "three" }); // => { three: "three" }
   deleteKeys(["four", "five", "one"], { 0: "one", 1: "two", 2: "three" }); // => { two: "two", three: "three" }
 
-
-//Write a function sum that returns the sum of all arguments passed to the function, make sure not to use parameters.
-
-const sum = function () {
-  let sum = 0
-      for (let i = 0; i < arguments.length; i++){
-          sum += arguments[i]
-       }
-       return sum
-};
-
-sum(1, 2); // => 3
-sum(1, 2, 3, 4); // => 10
-sum(4, 5, 6, 7, 8); // => 30
-sum(1); // => 1
-sum(); // => 0
+  const sumArguments = function () {
+   let sum =0
+   for (let index = 0; index < arguments.length; index++) {
+     sum += arguments[index]
+     
+   } 
+   return sum
+  };
+  sumArguments(1, 2); // => 3
+  sumArguments(1, 2, 3, 4); // => 10
+  sumArguments(4, 5, 6, 7, 8); // => 30
+  sumArguments(1); // => 1
+  sumArguments(); // => 0
